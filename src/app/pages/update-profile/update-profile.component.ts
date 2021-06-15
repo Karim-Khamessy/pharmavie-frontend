8c848d0310d7b1701a2263a4b147ea0645aa40d8
Sarah-Kamoun <sarah.f.kamoun@gmail.com>
2021-06-15 17:21:34 +0100

    update profile page v2

--- src/app/pages/update-profile/update-profile.component.ts
+++ src/app/pages/update-profile/update-profile.component.ts
@@ -13,4 +13,32 @@ export class UpdateProfileComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  verify() {
+   
+    var password = document.getElementById("password");
+    var confirm_password = document.getElementById("confirm_password");
+    var bttn=document.getElementById('update');
+    var status=document.getElementById('message');
+
+    function validatePassword(){
+
+      if(password == confirm_password)
+      {
+        bttn.removeAttribute("disabled");
+        status.style.color = 'green';
+        status.innerHTML = 'matching';
+
+      } else{
+        status.style.color = 'red';
+        status.innerHTML = 'not matching';
+        bttn.setAttribute("disabled", "disabled");
+      }
+      
+    }
+  
+    password.onchange = validatePassword;
+    confirm_password.onkeyup = validatePassword;
+    
+  }
+
 }
